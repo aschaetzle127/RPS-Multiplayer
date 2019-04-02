82e375093ba71bc1a2238a62ab53587a5cd13427
Lexi127 <aperalta127@gmail.com>
2019-04-01 18:24:30 -0700

    added JS

---
+++ assets/javascript/firebase.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyCNJ_DPLP31sYDRbogZGtbT6_qO5xZL-mw",
+    authDomain: "rps-multiplayer-cbf81.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-cbf81.firebaseio.com",
+    projectId: "rps-multiplayer-cbf81",
+    storageBucket: "rps-multiplayer-cbf81.appspot.com",
+    messagingSenderId: "1067881902570"
+  };
+  
+  firebase.initializeApp(config);
